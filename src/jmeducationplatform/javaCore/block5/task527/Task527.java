4a52629f8e48b01f543cd582cefc73c08b30516d
Hilkevich <mx190685@gmail.com>
2021-02-28 23:42:51 +0300

    принимаем InputStream и возвращаем сумму всех его элементов.

--- src/jmeducationplatform/javaCore/block5/task527/Task527.java
+++ src/jmeducationplatform/javaCore/block5/task527/Task527.java
@@ -1,4 +1,5 @@
 /*
+
 Напишите метод int sumOfStream(InputStream inputStream), который принимает InputStream и возвращает сумму всех его
 элементов.
 
@@ -21,42 +22,45 @@ public class Task527 {
 
     public static void main(String[] args) {
 
-//        InputStream input = null;
-//
-//        try {
-//            input = new FileInputStream("C:/1.txt");
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.print("input: " + input);
 
+        try {
+            InputStream input = new FileInputStream("E:/1.txt");    // создаем объект,проверяя его на исключения. Путь к реальному файлу на диске.
+
+            try {
+                sumOfStream(input);       // вызываем метод (подставив в него созданный объект),проверяя на исключения.
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public int sumOfStream(InputStream inputStream) throws IOException {
 
+    public static int sumOfStream(InputStream inputStream) throws IOException {
 
         int buf;
         int result = 0;
 
-     //   try {
+        try {
             while ((buf = inputStream.read()) != -1) {   // засовываем в b то, что вычитали из inputStream, но пока в нем есть что-то.(т.е не -1).
-                result = result + (byte)buf;             // приводим к byte, т.к в потоке могут быть отрицат. числа.(иначе выдает ошибку на платформе)
-                System.out.println("result:" + result);
+                result = result + (byte) buf;            // приводим к byte, т.к в потоке могут быть отрицат. числа.(иначе выдает ошибку на платформе)
+                System.out.print("|" + result);
             }
 
-       // } catch (IOException ignore) {
+        } catch (IOException ignore) {
 
-       // } finally {
+        } finally {
 
-           // try {
-              //  inputStream.close();
+            try {
+                inputStream.close();
 
-           // } catch (IOException ignore) {
+            } catch (IOException ignore) {
 
-           // }
-       // }
-
+            }
+        }
+        System.out.println("\nрезультат: " + result);
         return result;
 
     }
